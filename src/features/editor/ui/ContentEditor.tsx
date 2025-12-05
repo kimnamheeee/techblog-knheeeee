e1b4a0a63d8c68987a75c254fc8bab11a87dd252
kimnamheeee <nami200284@snu.ac.kr>
2025-12-06 03:18:17 +0900

    ✨ feat(editor): dual markdown support
    need to separate component

--- src/features/editor/ui/ContentEditor.tsx
+++ src/features/editor/ui/ContentEditor.tsx
@@ -2,24 +2,63 @@
 
 import { useEditor, EditorContent } from "@tiptap/react";
 import { Placeholder } from "@tiptap/extensions";
-import StarterKit from "@tiptap/starter-kit";
-import Typography from "@tiptap/extension-typography";
-import Highlight from "@tiptap/extension-highlight";
 import { Markdown } from "@tiptap/markdown";
+import Document from "@tiptap/extension-document";
+import Paragraph from "@tiptap/extension-paragraph";
+import Text from "@tiptap/extension-text";
+import StarterKit from "@tiptap/starter-kit";
+import Heading from "@tiptap/extension-heading";
+
+import { useState, useEffect } from "react";
 
 export default function ContentEditor() {
+  const [markdownText, setMarkdownText] = useState("");
+
   const editor = useEditor({
     extensions: [
-      StarterKit,
+      Document,
+      Paragraph,
+      Text,
       Placeholder.configure({
         placeholder: "내용을 입력하세요...",
       }),
-      Typography,
-      Highlight,
+    ],
+    immediatelyRender: false,
+    onUpdate: ({ editor }) => {
+      const markdownText = editor.getText();
+      setMarkdownText(markdownText);
+    },
+  });
+
+  const editor2 = useEditor({
+    extensions: [
+      StarterKit,
       Markdown,
+      Placeholder.configure({
+        placeholder: "내용을 입력하세요...",
+      }),
     ],
     immediatelyRender: false,
   });
 
-  return <EditorContent editor={editor} />;
+  useEffect(() => {
+    const parseMarkdown = () => {
+      if (!editor2 || !editor2.markdown) {
+        return;
+      }
+      try {
+        editor2.commands.setContent(markdownText, { contentType: "markdown" });
+      } catch (err) {
+        console.error(err);
+      }
+    };
+    parseMarkdown();
+  }, [markdownText]);
+
+  return (
+    <>
+      <EditorContent editor={editor} />
+      <EditorContent editor={editor2} />
+    </>
+  );
 }
