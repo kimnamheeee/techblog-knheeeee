7a431d2bc7e4c2ce49e319288381bc333c21137d
kimnamheeee <nami200284@snu.ac.kr>
2025-12-05 23:38:57 +0900

    💎 style(header): adjust style

--- src/widgets/Header.tsx
+++ src/widgets/Header.tsx
@@ -1,11 +1,11 @@
 import Link from "next/link";
 
 export default function Header() {
-    return (
-        <header className="flex px-6 py-4 justify-between items-center">
-            <Link href="/">
-                <h1 className="text-2xl font-bold">Tech Blog</h1>
-            </Link>
-        </header>
-    )
+  return (
+    <header className="flex px-8 py-6 justify-between items-center">
+      <Link href="/">
+        <h1 className="text-2xl font-bold">Tech Blog</h1>
+      </Link>
+    </header>
+  );
 }
